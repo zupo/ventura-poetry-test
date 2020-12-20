20aae4ac03212a7bab673798e552101c1b9a55f9
Nejc Zupan <nejc.zupan@gmail.com>
2020-12-20 18:45:24 +0000

    initial PoC

---
+++ shell.nix
@@ -0,0 +1,21 @@
+let
+  nixpkgs = builtins.fetchTarball {
+    # https://github.com/NixOS/nixpkgs/tree/nixos-20.09 on 2020-12-19
+    url = "https://github.com/nixos/nixpkgs/archive/7d57790bc12edaa8cbb76c38b28b59535492864e.tar.gz";
+    sha256 = "1pkms2l3fxhn4mpxmknkirbxzgvmhg5qz4mlgn12aqsn3mxf4ag4";
+  };
+  pkgs = import nixpkgs { config = { allowUnfree = true; }; };
+  env = pkgs.poetry2nix.mkPoetryEnv {
+    pyproject = ./pyproject.toml;
+    poetrylock = ./poetry.lock;
+  };
+in
+
+pkgs.mkShell {
+  name = "dev-shell";
+  buildInputs = [
+    env
+    pkgs.poetry
+  ];
+
+}
