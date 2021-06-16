ea6543f5ea1103f07824f5960fdd8994d4989ba8
Nejc Zupan <nejc.zupan@gmail.com>
2021-06-16 11:11:06 +0200

    also pin poetry2nix version

--- shell.nix
+++ shell.nix
@@ -1,22 +1,28 @@
 let
   nixpkgs = builtins.fetchTarball {
-    # https://github.com/NixOS/nixpkgs/tree/nixos-21.05 on 2021-06-09
-    url = "https://github.com/nixos/nixpkgs/archive/5de44c15758465f8ddf84d541ba300b48e56eda4.tar.gz";
-    sha256 = "sha256:05darjv3zc5lfqx9ck7by6p90xgbgs1ni6193pw5zvi7xp2qlg4x";
+    # https://status.nixos.org/ -> nixos-21.05 on 2021-06-16
+    url = "https://github.com/nixos/nixpkgs/archive/1f91fd1040667e9265a760b0347f8bc416249da7.tar.gz";
   };
-  pkgs = import nixpkgs { config = { allowUnfree = true; }; };
+  pkgs = import nixpkgs {};
+  poetry2nix = import (fetchTarball {
+    # https://github.com/nix-community/poetry2nix/commits/master on 2021-06-16
+    url = "https://github.com/nix-community/poetry2nix/archive/74303472ac116fa21452f2fcd8d3a8e78d174268.tar.gz";
+  }) {
+    pkgs = pkgs;
+    poetry = pkgs.poetry;
+  };
 
   poetryOverrides = self: super: {
   };
 
   commonPoetryArgs = {
     overrides = [
-      pkgs.poetry2nix.defaultPoetryOverrides
+      poetry2nix.defaultPoetryOverrides
       poetryOverrides
     ];
   };
 
-  env = pkgs.poetry2nix.mkPoetryEnv ( commonPoetryArgs // {
+  env = poetry2nix.mkPoetryEnv ( commonPoetryArgs // {
     pyproject = ./pyproject.toml;
     poetrylock = ./poetry.lock;
   });
