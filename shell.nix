3a8b74148a8ca5cb5cbfe3704b21633daed42940
Nejc Zupan <nejc.zupan@gmail.com>
2022-01-19 23:31:02 +0000

    works on 21.05

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 let
   nixpkgs = builtins.fetchTarball {
-    # https://status.nixos.org/ -> nixos-21.11 on 2022-01-19
-    url = "https://github.com/nixos/nixpkgs/archive/610d4ea2750e064bf34b33fa38cb671edd893d3d.tar.gz";
+    # https://status.nixos.org/ -> nixos-21.05 on 2022-01-19
+    url = "https://github.com/nixos/nixpkgs/archive/0fd9ee1aa36ce865ad273f4f07fdc093adeb5c00.tar.gz";
   };
   pkgs = import nixpkgs {};
   poetry2nix = import (fetchTarball {
